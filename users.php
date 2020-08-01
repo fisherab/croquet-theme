1d1a8ec95e77cf46cb7b19bdb80cdd9c2d7f6901
Steve Fisher <dr.s.m.fisher@gmail.com>
2020-08-01 21:02:56 +0100

    Use avatar in preference

--- users.php
+++ users.php
@@ -32,7 +32,7 @@ if ( is_user_logged_in() and is_user_member_of_blog()) {
 				$phone = $phone_1;
 				if ($phone_2 != "") $phone = $phone . ' / ' . $phone_2;
 				$img = get_cupp_meta($user -> id, 'thumbnail');
-				if ($img == "") $img = get_stylesheet_directory_uri() . '/images/question.jpg';
+				$img = get_avatar_url($user -> id, ['default' => $img]);
 				$hcap = "";
 				$ac = get_user_meta($user->id, 'ac_h', true);
 				$gc = get_user_meta($user->id, 'gc_h', true);
